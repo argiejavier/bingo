08291ac2d953ab4fd8bdaae5b7ebb36735cc93db
argiejavier <argiejavier622@gmail.com>
2024-03-14 14:35:19 -0700

    My Second Commit.

--- index.js
+++ index.js
@@ -12,12 +12,23 @@ function generateNumber(max) {
 
     // if ... else if ... else
     // GT >, GTE >=, LT <, LTE <=, EQ ==, NEQ !=, AND &&, OR ||
-    if (randomNumber <= 15) {
-        console.log(`The number is in B.`);
-    } else if (randomNumber >= 16 && randomNumber <= 30) {
-        console.log(`The number is in I.`);
-    } else {
-        console.log(`The number is invalid`);
+    if (randomNumber <= 15){
+        console.log('the Number is in B.');
+    }
+    else if(randomNumber >= 16 && randomNumber <= 30){
+        console.log('the number is in I');
+    }
+    else if(randomNumber >= 31 && randomNumber <= 45){
+        console.log('the number is in N');
+    }
+    else if(randomNumber >= 46 && randomNumber <= 60){
+        console.log('the number is in G');
+    }
+    else if(randomNumber >= 61 && randomNumber <= 75){
+        console.log('the number is in O');
+    }
+    else{
+        console.log('the number is invalid');
     }
 
     return console.log(randomNumber);
